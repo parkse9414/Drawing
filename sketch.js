46178c75d8fa53436c15145663f42f19fe0af8af
박세원 <parkse9414@naver.com>
2014-09-11 13:36:28 +0900

    gdgdfg

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+ //0: balck, 255; white
+//background(red, green, blue);
+
+  background(255.0.0);
   stroke(255);
   noFill();
+
   line(10, 10, 90, 10);
     rect(20, 20, 80, 50);
     ellipse(50, 50, 50, 50);
